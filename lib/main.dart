1dee6968ef27ae089291d44354d19910c0ef3b7d
Zwe Thura Aung <72401613+zwethu@users.noreply.github.com>
2021-08-27 21:53:07 +0630

    testing 4

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hi',
+      title: 'Eco Ml',
+    
     );
   }
 }
